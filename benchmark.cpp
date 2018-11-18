45349527506b10864f250c01e3d93a0c956a0654
jcf94 <xff252595680@gmail.com>
2018-11-18 18:24:46 +0800

    Add time & bandwidth count

--- benchmark.cpp
+++ benchmark.cpp
@@ -19,9 +19,9 @@ PROG   : benchmark
 
 using namespace std;
 
-#define KB 1024
-#define MB 1024 * KB
-#define GB 1024 * MB
+#define KB (1024)
+#define MB (1024 * KB)
+#define GB (1024 * MB)
 
 int main(int argc, char* argv[])
 {
@@ -50,25 +50,44 @@ int main(int argc, char* argv[])
         RDMA_Endpoint* endpoint = session.ptp_connect(&pre_tcp);
 
         // Prepare data
-        int total_data = 512 * MB;
-        int block_data = 64 * KB;
+        int total_data = 256 * MB;
+        int block_data = 512 * KB;
 
         if (strcmp(argv[1], "s") == 0)
         {
-            while (endpoint->total_recv_data() < total_data)
+            while (endpoint->total_recv_data() < total_data*2)
             {
                 //log_ok(endpoint->total_recv_data());
                 this_thread::sleep_for(10ms);
             }
             log_ok("recv over");
-            endpoint->close();
+            //endpoint->close();
         } else if (strcmp(argv[1], "c") == 0)
         {
             char* test_data = (char*)malloc(total_data);
+
+            // Warm Up
+
             for (int i=0;i<total_data;i+=block_data)
             {
                 endpoint->send_data((void*)(test_data+i), block_data);
             }
+
+            // Test Start
+
+            std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+
+            for (int i=0;i<total_data;i+=block_data)
+            {
+                endpoint->send_data((void*)(test_data+i), block_data);
+            }
+
+            std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+            log_ok(make_string("Send Over, Total %lld Bytes with Block size %lld Bytes", total_data, block_data));
+            log_ok(make_string("Total Time used: %lld ms, Bandwidth: %lf MB/s", duration, (double)(total_data / MB) / duration * 1000));
+
+            //endpoint->close();
         }
     }
     
